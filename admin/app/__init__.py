43acfd8ab1b37b5e23af35a375423d8fd1c9956a
c10t <c10t@fa.ke>
2018-08-05 00:00:19 +0900

    add error 400

--- admin/app/__init__.py
+++ admin/app/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+from flask import Flask, jsonify
+
 app = Flask(__name__,
     static_folder=os.getcwd() + '/app/public',
     static_url_path='')
@@ -9,3 +11,7 @@ app.config.from_pyfile('config.py', silent=True)
 @app.route('/')
 def root:
     return app.send_static_file('index.html')
+
+@APP.errorhandler(400)
+def bad_request(err):
+    return jsonify(error='400 ERROR'), 400
